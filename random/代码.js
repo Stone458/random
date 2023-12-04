e0d5eef1aa46d20298c2b604a917b0ce06325fb7
Stone458 <152880666+Stone458@users.noreply.github.com>
2023-12-04 22:28:22 +0800

    Add files via upload

---
+++ random/代码.js
@@ -0,0 +1,16 @@
+function 随机事件()
+{
+    var a=Math.round(Math.random()*6);
+    if(a==1)
+    alert("啥都不干")
+else if(a==2)
+alert("看本")
+else if(a==3)
+alert("打胶")
+else if(a==4)
+alert("练琴")
+else if(a==5)
+alert("学习")
+else if(a==6)
+alert("水群")
+}
